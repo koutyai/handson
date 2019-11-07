a11e3f80ac27cba9bcf7340941aa2e6f0826ce91
渡邉弘大 <koutyai@watanabekoudai-no-MacBook-Air.local>
2019-11-07 10:05:36 +0900

    to 4.4

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,7 +1,25 @@
+from functools import wraps
 from flask import request, redirect, url_for, render_template, flash, abort, \
-        jsonify, session
+        jsonify, session, g
 from flaskr import app, db
 from flaskr.models import Entry, User
+
+def login_required(f):
+    @wraps(f)
+    def decorated_view(*args, **kwargs):
+        if g.user is None:
+            return redirect(url_for('login', next=request.path))
+        return f(*args, **kwargs)
+    return decorated_view
+
+@app.before_request
+def load_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(session['user_id'])
+
 
 @app.route('/')
 def show_entries():
@@ -21,16 +39,19 @@ def add_entry():
 
 
 @app.route('/users/')
+@login_required
 def user_list():
     users = User.query.all()
     return render_template('user/list.html', users=users)
 
 @app.route('/users/<int:user_id>/')
+@login_required
 def user_detail(user_id):
     user = User.query.get(user_id)
     return render_template('user/detail.html', user=user)
 
 @app.route('/users/<int:user_id>/edit/', methods=['GET', 'POST'])
+@login_required
 def user_edit(user_id):
     user = User.query.get(user_id)
     if user is None:
@@ -38,17 +59,19 @@ def user_edit(user_id):
     if request.method == 'POST':
         user.name=request.form['name']
         user.email=request.form['email']
-        user.password=request.form['password']
-        db.session.add(user)
+        if request.form['password']:
+            user.password=request.form['password']
+        #db.session.add(user)
         db.session.commit()
         return redirect(url_for('user_detail', user_id=user_id))
     return render_template('user/edit.html', user=user)
 
 @app.route('/users/create/', methods=['GET', 'POST'])
+@login_required
 def user_create():
     if request.method == 'POST':
         user = User(name=request.form['name'],
-                    email=request.form['email'],
+                    email=request.form['email'], 
                     password=request.form['password'])
         db.session.add(user)
         db.session.commit()
@@ -66,10 +89,11 @@ def user_delete(user_id):
     db.session.commit()
     return jsonify({'status': 'OK'})
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user, authenticated = User.authenticate(db.session.query,
+        user, authenticated = User.authenticate(db.session.query, 
                 request.form['email'], request.form['password'])
         if authenticated:
             session['user_id'] = user.id
@@ -84,4 +108,3 @@ def logout():
     session.pop('user_id', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
-
